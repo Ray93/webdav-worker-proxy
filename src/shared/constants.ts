2673f271155089fcd28cd7c59159404041e4070c
Ray <xurui93@qq.com>
2026-04-18 08:24:08 +0800

    chore: add lockfile and ignore scaffold artifacts

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,4 +1,4 @@
 export const ADMIN_SECRET_NAME = "ADMIN_SESSION_SECRET";
 export const ADMIN_UI_PREFIX = "/admin";
 export const ADMIN_API_PREFIX = "/api/admin";
-export const RESERVED_PREFIXES = [ADMIN_UI_PREFIX, ADMIN_API_PREFIX];
+export const RESERVED_PREFIXES = [ADMIN_UI_PREFIX, ADMIN_API_PREFIX] as const;
